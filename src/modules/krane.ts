bf567927954abf2123b6c8682e9fb13f6f02b35f
Lleyton Gray <lleyton@fyralabs.com>
2024-04-21 07:03:17 +0000

    fix: make krane module send sticker

--- src/modules/krane.ts
+++ src/modules/krane.ts
@@ -3,6 +3,8 @@ import { Events } from 'discord.js';
 
 client.on(Events.MessageCreate, async (message) => {
   if (message.content.match(/\bkrane\b/)?.length) {
-    await message.reply("https://media.discordapp.net/stickers/1229211273904787531.png");
+    await message.reply({
+      stickers: ['1229211273904787531'],
+    });
   }
 });
