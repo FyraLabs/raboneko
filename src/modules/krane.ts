c5850058a58708c57b8c32e970013e040e388df3
WeirdTreeThing <bradyn127@protonmail.com>
2024-04-21 00:56:41 -0400

    Fix something in the krane module
    supposedly this fixes something

--- src/modules/krane.ts
+++ src/modules/krane.ts
@@ -2,7 +2,7 @@ import client from '../client';
 import { Events } from 'discord.js';
 
 client.on(Events.MessageCreate, async (message) => {
-  if (message.content.match(/\bkrane\b/).length) {
+  if (message.content.match(/\bkrane\b/)?.length) {
     await message.reply("https://media.discordapp.net/stickers/1229211273904787531.png");
   }
 });
