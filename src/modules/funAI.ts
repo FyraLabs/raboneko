ea5264808b66aec6d2cda23b6807cb90a802235f
madomado <madonuko@outlook.com>
2024-04-21 18:07:40 +0800

    fix: random errros

--- src/modules/funAI.ts
+++ src/modules/funAI.ts
@@ -1,7 +1,7 @@
 import client from '../client';
 import { Events, Message } from 'discord.js';
 
-function containsWord(msg: Message, word: string): bool {
+function containsWord(msg: Message, word: string): boolean {
   return msg.content.match(`/\\b${word}\\b/i`)?.length !== 0;
 }
 
@@ -11,7 +11,7 @@ client.on(Events.MessageCreate, async (message) => {
       stickers: ['1229211273904787531'],
     });
   }
-  if (message.content.toLowerCase() == 'skull' || message.containsWord(message, 'skull\\s+emoji')) {
+  if (message.content.toLowerCase() == 'skull' || containsWord(message, 'skull\\s+emoji')) {
     await message.react('💀');
   }
 });
