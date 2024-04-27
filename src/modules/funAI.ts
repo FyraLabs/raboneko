bf05f3ef43a3c8970d1e06781f6ff42cc73e8c75
WeirdTreeThing <bradyn127@protonmail.com>
2024-04-27 01:16:08 -0400

    feat: add hayato reaction

--- src/modules/funAI.ts
+++ src/modules/funAI.ts
@@ -8,6 +8,9 @@ client.on(Events.MessageCreate, async (message) => {
   if (containsWord(message, 'krane')) {
     await message.react('1233642528889245776');
   }
+  if (containsWord(message, 'hayato')) {
+    await message.react('1231664960032215070');
+  }
   if (message.content.toLowerCase() == 'skull' || containsWord(message, 'skull\\s+emoji')) {
     await message.react('💀');
   }
