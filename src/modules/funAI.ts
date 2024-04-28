9665f97456c826b531d7d3c8f6e0fa9e68d261e8
lea <lleyton@fyralabs.com>
2024-04-27 19:26:34 -0700

    HOTFIX OH FUCK

--- src/modules/funAI.ts
+++ src/modules/funAI.ts
@@ -1,8 +1,10 @@
 import client from '../client';
 import { Events, Message } from 'discord.js';
 
-const containsWord = (msg: Message, word: string): boolean =>
-  msg.content.match(`/\\b${word}\\b/i`)?.length !== 0;
+const containsWord = (msg: Message, word: string): boolean => {
+  const matches = msg.content.match(new RegExp(`\\b${word}\\b`, 'i'));
+  return matches != null && matches.length > 0;
+};
 
 client.on(Events.MessageCreate, async (message) => {
   if (containsWord(message, 'krane')) {
