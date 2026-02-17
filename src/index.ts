4a041ce437003245a24685d6f95922c90ae7a7bb
Lleyton Gray <lleyton@fyralabs.com>
2026-02-17 12:45:43 -0800

    disable tex

--- src/index.ts
+++ src/index.ts
@@ -59,5 +59,5 @@ import './modules/funAI';
 import './modules/cutefishInfo';
 import './modules/support';
 import './modules/logger';
-import './modules/tex';
+// import './modules/tex'; disabled for now
 import './modules/wraps';
