990159071796c1fd53e0c77cb4f61794f65db432
Lleyton Gray <lleyton@fyralabs.com>
2026-07-30 16:29:14 -0700

    fix: prisma load bullshit

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,13 @@
-import "@std/dotenv/load";
+import process from "node:process";
 import { defineConfig, env } from "prisma/config";
 
-// ./prisma.config.ts
+// Prisma is weird and does not like @std/dotenv/load, so /shrug
+try {
+  process.loadEnvFile();
+} catch {
+  // We aren't able to load the .env file, that's fine :)
+}
+
 export default defineConfig({
   datasource: {
     url: env("DATABASE_URL"),
